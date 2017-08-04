abd0b3ec74bd602989b7d7dd3c9107d04cdd74e6
Stephen J Day <stephen.day@docker.com>
2017-08-04 12:17:51 -0700

    protobuild: add support for descriptor aggregation
    This change introduces the ability to aggregate descriptors into
    arbitrary collections under project root. Descriptors outline the
    services and messages that make up a GRPC service. Aggregating all of
    the GRPC service definitions into a single file provides output that can
    be used to ensure that changes don't impact API stability.
    
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

---
+++ descriptors.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+)
+
+type descriptorSet struct {
+	merged      descriptor.FileDescriptorSet
+	seen        map[string]struct{}
+	ignoreFiles map[string]struct{}
+}
+
+func newDescriptorSet(ignoreFiles ...string) *descriptorSet {
+	ifm := make(map[string]struct{}, len(ignoreFiles))
+	for _, ignore := range ignoreFiles {
+		ifm[ignore] = struct{}{}
+	}
+	return &descriptorSet{
+		seen:        make(map[string]struct{}),
+		ignoreFiles: ifm,
+	}
+}
+
+func (d *descriptorSet) add(descs ...*descriptor.FileDescriptorProto) {
+	for _, file := range descs {
+		name := file.GetName()
+		if _, ok := d.seen[name]; ok {
+			continue
+		}
+
+		if _, ok := d.ignoreFiles[name]; ok {
+			continue
+		}
+
+		// TODO(stevvooe): If we want to filter certain fields in the descriptor,
+		// this is the place to do it. May be necessary if certain fields are
+		// noisy, such as option fields.
+		d.merged.File = append(d.merged.File, file)
+		d.seen[name] = struct{}{}
+	}
+}
+
+// stabilize outputs the merged protobuf descriptor set into the provided writer.
+//
+// This is equivalent to the following command:
+//
+// cat merged.pb | protoc --decode google.protobuf.FileDescriptorSet /usr/local/include/google/protobuf/descriptor.proto
+func (d *descriptorSet) marshalTo(w io.Writer) error {
+	p, err := proto.Marshal(&d.merged)
+	if err != nil {
+		return err
+	}
+
+	args := []string{
+		"protoc",
+		"--decode",
+		"google.protobuf.FileDescriptorSet",
+
+		// TODO(stevvooe): Come up with better way to resolve this path.
+		"/usr/local/include/google/protobuf/descriptor.proto",
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdin = bytes.NewReader(p)
+	cmd.Stdout = w
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func readDesc(path string) (*descriptor.FileDescriptorSet, error) {
+	var desc descriptor.FileDescriptorSet
+
+	p, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := proto.Unmarshal(p, &desc); err != nil {
+		log.Fatalln(err)
+	}
+
+	return &desc, nil
+}
