containerd/protobuild

commit 95af7b79899679e53fdc3a2b278e16af6d163bd1

Don't hardcode path to google/protobuf/descriptor.proto
Despite the ability to specify multiple include directories
in `config.Include.After`, the `/usr/local/bin` path to
`google/protobuf/descriptor.proto` is hardcoded in
`func (*descriptorSet).marshalTo()`.

Fix by introducing a function to search for this file,
and providing the path found to newDesceriptorSet.

This (together with the addition of `/usr/include` path to
`config.Include.After`) makes it possible to use protobuilder
with distro-installed protoc (e.g. with Debian/Ubuntu it is
`apt install libprotobuf-dev protobuf-compiler`).

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

descriptors.go
@@ -18,9 +18,10 @@ type descriptorSet struct {
 	merged      descriptor.FileDescriptorSet
 	seen        map[string]struct{}
 	ignoreFiles map[string]struct{}
+	descProto   string
 }
 
-func newDescriptorSet(ignoreFiles ...string) *descriptorSet {
+func newDescriptorSet(ignoreFiles []string, d string) *descriptorSet {
 	ifm := make(map[string]struct{}, len(ignoreFiles))
 	for _, ignore := range ignoreFiles {
 		ifm[ignore] = struct{}{}
@@ -28,6 +29,7 @@ func newDescriptorSet(ignoreFiles ...string) *descriptorSet {
 	return &descriptorSet{
 		seen:        make(map[string]struct{}),
 		ignoreFiles: ifm,
+		descProto:   d,
 	}
 }
 
@@ -54,7 +56,7 @@ func (d *descriptorSet) add(descs ...*descriptor.FileDescriptorProto) {
 //
 // This is equivalent to the following command:
 //
-// cat merged.pb | protoc --decode google.protobuf.FileDescriptorSet /usr/local/include/google/protobuf/descriptor.proto
+// cat merged.pb | protoc --decode google.protobuf.FileDescriptorSet /path/to/google/protobuf/descriptor.proto
 func (d *descriptorSet) marshalTo(w io.Writer) error {
 	p, err := proto.Marshal(&d.merged)
 	if err != nil {
@@ -65,10 +67,7 @@ func (d *descriptorSet) marshalTo(w io.Writer) error {
 		"protoc",
 		"--decode",
 		"google.protobuf.FileDescriptorSet",
-
-		// TODO(stevvooe): Come up with better way to resolve this path.
-		"-I/usr/local/include",
-		"/usr/local/include/google/protobuf/descriptor.proto",
+		d.descProto,
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)

main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
 	"syscall"
@@ -69,10 +70,15 @@ func main() {
 		}
 	}
 
+	descProto, err := descriptorProto(c.Includes.After)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	// Aggregate descriptors for each descriptor prefix.
 	descriptorSets := map[string]*descriptorSet{}
 	for _, stable := range c.Descriptors {
-		descriptorSets[stable.Prefix] = newDescriptorSet(stable.IgnoreFiles...)
+		descriptorSets[stable.Prefix] = newDescriptorSet(stable.IgnoreFiles, descProto)
 	}
 
 	shouldGenerateDescriptors := func(p string) bool {
@@ -331,6 +337,22 @@ func gopathJoin(gopath, element string) string {
 	}
 
 	return strings.Join(elements, string(filepath.ListSeparator))
+}
+
+// descriptorProto returns the full path to google/protobuf/descriptor.proto
+// which might be different depending on whether it was installed. The argument
+// is the list of paths to check.
+func descriptorProto(paths []string) (string, error) {
+	const descProto = "google/protobuf/descriptor.proto"
+
+	for _, dir := range paths {
+		file := path.Join(dir, descProto)
+		if _, err := os.Stat(file); err == nil {
+			return file, err
+		}
+	}
+
+	return "", fmt.Errorf("File %q not found (looked in: %v)", descProto, paths)
 }
 
 var errVendorNotFound = fmt.Errorf("no vendor dir found")

Protobuild.toml
@@ -44,9 +44,9 @@ plugins = ["grpc"]
   # packages = ["github.com/gogo/protobuf"]
 
   # Paths that will be added untouched to the end of the includes. We use
-  # `/usr/local/include` to pickup the common install location of protobuf.
+  # a few paths to pickup the common install locations for protobuf.
   # This is the default.
-  # after = ["usr/local/include"]
+  # after = ["usr/local/include", "/usr/include"]
 
 # This section let's us map protobuf imports to Go packages. These will become
 # `-M` directives in the call to the go protobuf generator.

config.go
@@ -51,7 +51,7 @@ func newDefaultConfig() config {
 			After    []string
 		}{
 			Before: []string{"."},
-			After:  []string{"/usr/local/include"},
+			After:  []string{"/usr/local/include", "/usr/include"},
 		},
 	}
 }

commit cf52757ef9fd5eb52273781aa42b53921f2cb61c

Fix running protoc
A recent commit caused a regression:

> protoc --decode google.protobuf.FileDescriptorSet /usr/local/include/google/protobuf/descriptor.proto
> /usr/local/include/google/protobuf/descriptor.proto: File does not reside within any path specified using --proto_path (or -I). You must specify a --proto_path which encompasses this file. Note that the proto_path must be an exact prefix of the .proto file names -- protoc is too dumb to figure out when two paths (e.g. absolute and relative) are equivalent (it's harder than you think).

Although I can't seem to reproduce it locally, it is clear that
protoc do require --proto_path in this case. Add it back.

Fixes: 95af7b79899679e5

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

descriptors.go
@@ -19,9 +19,10 @@ type descriptorSet struct {
 	seen        map[string]struct{}
 	ignoreFiles map[string]struct{}
 	descProto   string
+	protoPath   string
 }
 
-func newDescriptorSet(ignoreFiles []string, d string) *descriptorSet {
+func newDescriptorSet(ignoreFiles []string, d, p string) *descriptorSet {
 	ifm := make(map[string]struct{}, len(ignoreFiles))
 	for _, ignore := range ignoreFiles {
 		ifm[ignore] = struct{}{}
@@ -30,6 +31,7 @@ func newDescriptorSet(ignoreFiles []string, d string) *descriptorSet {
 		seen:        make(map[string]struct{}),
 		ignoreFiles: ifm,
 		descProto:   d,
+		protoPath:   p,
 	}
 }
 
@@ -67,6 +69,8 @@ func (d *descriptorSet) marshalTo(w io.Writer) error {
 		"protoc",
 		"--decode",
 		"google.protobuf.FileDescriptorSet",
+		"--proto_path",
+		d.protoPath,
 		d.descProto,
 	}
 

main.go
@@ -70,7 +70,7 @@ func main() {
 		}
 	}
 
-	descProto, err := descriptorProto(c.Includes.After)
+	descProto, protoPath, err := descriptorProto(c.Includes.After)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -78,7 +78,7 @@ func main() {
 	// Aggregate descriptors for each descriptor prefix.
 	descriptorSets := map[string]*descriptorSet{}
 	for _, stable := range c.Descriptors {
-		descriptorSets[stable.Prefix] = newDescriptorSet(stable.IgnoreFiles, descProto)
+		descriptorSets[stable.Prefix] = newDescriptorSet(stable.IgnoreFiles, descProto, protoPath)
 	}
 
 	shouldGenerateDescriptors := func(p string) bool {
@@ -340,19 +340,19 @@ func gopathJoin(gopath, element string) string {
 }
 
 // descriptorProto returns the full path to google/protobuf/descriptor.proto
-// which might be different depending on whether it was installed. The argument
-// is the list of paths to check.
-func descriptorProto(paths []string) (string, error) {
+// (which might be different depending on whether it was installed), and
+// the path where it was found. The argument is the list of paths to check.
+func descriptorProto(paths []string) (string, string, error) {
 	const descProto = "google/protobuf/descriptor.proto"
 
 	for _, dir := range paths {
 		file := path.Join(dir, descProto)
 		if _, err := os.Stat(file); err == nil {
-			return file, err
+			return file, dir, err
 		}
 	}
 
-	return "", fmt.Errorf("File %q not found (looked in: %v)", descProto, paths)
+	return "", "", fmt.Errorf("File %q not found (looked in: %v)", descProto, paths)
 }
 
 var errVendorNotFound = fmt.Errorf("no vendor dir found")